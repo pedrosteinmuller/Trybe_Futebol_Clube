28bbd60947db839a53ba425d9e0d56cd613db250
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G>
2023-02-27 16:51:40 -0300

    feat: criacao da migration teams

---
+++ app/backend/src/database/migrations/20230227194846-teams.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      team_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('teams');
+  }
+};
